91928727b86124328751b67db93aae568d4bf538
Sonu Kumar <deepakkumar23887@gmail.com>
2025-04-26 18:34:57 +0530

    Add main.py

---
+++ main.py
@@ -0,0 +1,81 @@
+import streamlit as st
+import pdfplumber
+import google.generativeai as genai
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+
+API_KEY = os.getenv("GOOGLE_API_KEY")
+genai.configure(api_key=API_KEY)
+
+st.set_page_config(page_title='AI Resume Analyzer', page_icon='assests/icon.png',layout='wide')
+
+st.title("AI Resume Analyzer")
+
+uploaded_resume = st.file_uploader("Upload Your Resume (pdf)",type=['pdf'])
+if uploaded_resume is not None:
+    st.session_state['resume_file'] = uploaded_resume
+
+if 'resume_file' in st.session_state:
+    with pdfplumber.open(st.session_state['resume_file']) as resume:
+        content = "".join([page.ectract_text() for page in resume.pages])
+
+
+job_description = st.text_area("Paste the Job Description here:")
+if job_description is not None:
+    st.session_state['job_description'] = job_description
+
+if 'job_description' in st.session_state:
+    job_des = st.session_state['job_description']
+
+
+prompt = f"""
+You are an expert career consultant and hiring manager.
+Analyze the following two inputs carefully:
+
+Resume Text:
+{content}
+
+Job Description:
+{job_des}
+
+Your tasks:
+
+First, calculate the probability (0% to 100%) of how well the resume matches the job description.
+
+Then, give a detailed analysis covering:
+
+Skills: 
+List important skills required in the Job Description.
+For each required skill:
+Check if it is exactly present in the resume.
+If not exactly present, check if a similar or related skill is present.
+If missing or only partially matched, clearly point it out.
+
+Projects: 
+Identify and review all projects mentioned in the resume.
+For each project:
+Analyze if the project description is well-written:
+Does it clearly explain what was built?
+Does it mention tools, technologies, results, and impact?
+If not, suggest exactly how the project description should be improved.
+Check if the project is aligned with the job role:
+If a project is unrelated, suggest either:
+A new relevant project idea.
+Or a way to reframe the project description to highlight transferable skills.
+
+Improvements:
+How can the candidate rewrite or improve their project descriptions to align better with the job?
+Mention specific keywords that should appear in the resume to improve matching.
+
+Final Recommendation: Summarize whether the candidate should apply now, or first improve their resume in one short paragraph.
+
+Output format:
+Matching Probability: __%
+Missing Skills: (if any otherwise not)
+Suggested Projects: (if any otherwise not)
+Resume Improvement Tips:
+Final Recommendation:
+"""
+
